c61f3e05fde61e1ac7ede2bbd130fdd14aa11631
Raiyan Hasan <raiyanhasan2006@gmail.com>
2026-08-18 11:19:41 +0530

    chore(react): bump @personaai/react to v0.1.1

--- sdk/react/src/index.ts
+++ sdk/react/src/index.ts
@@ -6,4 +6,4 @@ export * from './hooks/useThreads.js';
 export * from './hooks/useAgents.js';
 export * from './hooks/useConnection.js';
 
-export const VERSION = '0.1.0';
+export const VERSION = '0.1.1';
